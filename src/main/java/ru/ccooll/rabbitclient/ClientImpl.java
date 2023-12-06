76b05b36597c3489556c39572863aca2f9ac546a
Cco0lL <cco0ll.nicko1998@gmail.com>
2023-12-06 22:49:48 +0500

    removed todo

--- src/main/java/ru/ccooll/rabbitclient/ClientImpl.java
+++ src/main/java/ru/ccooll/rabbitclient/ClientImpl.java
@@ -10,9 +10,6 @@ import ru.ccooll.rabbitclient.error.ErrorHandler;
 
 import java.util.concurrent.ExecutorService;
 
-
-//TODO: придумать как сделать крутую фабрику для пропертис с разным контентом, (application/octet-stream,
-// text/plain, application/json и с delivery mode (1 - default, 2 - persists)
 public record ClientImpl(ConnectionFactory connectionFactory, ExecutorService clientWorker,
                          Serializer defaultSerializer, Deserializer defaultDeserializer,
                          ErrorHandler errorHandler, Connection connection) implements Client {
